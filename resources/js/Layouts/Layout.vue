3150c3f494dd995f286abd4d907ad12130904165
Jhovanie Flores <jhovanieflores@Jhovanies-MacBook-Air.local>
2025-02-20 14:59:55 +0800

    update-placement-footer

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -1,21 +1,10 @@
 <script setup>
-import NavBar from "../Components/NavBar.vue";
 import Footer from "../Components/Footer.vue";
-import { usePage } from "@inertiajs/vue3";
-import AuthNavBar from "../Components/AuthNavBar.vue";
-import { computed } from "vue";
 import CombineNavbar from "../Components/CombineNavbar.vue";
-
-
-// const page = usePage().props.auth;
-// const auth = computed( () => page);
-
-const auth = computed(() => usePage().props.auth);
-
 </script>
 
 <template>
-    <div class="min-h-screen">
+    <div class="min-h-screen flex flex-col">
         <header class="bg-green-700 text-white">
             <CombineNavbar />
         </header>
